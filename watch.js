13cb64f20794afde12e6dd1544125abb87b6e8f5
Damian Galeano <dj.damian.b@hotmail.com>
2018-07-11 17:38:38 -0500

    Update Hours minutes and seconds

--- watch.js
+++ watch.js
@@ -1,17 +1,17 @@
 (function () {
     var updateHour = function() {
-        var date = new Date();
-            hours = date.getHours();
+        var date = new Date(),
+            hours = date.getHours(),
             ampm,
-            minutes = date.getMinutes();
-            seconds = date.getSeconds();
-            dayWeek = date.getDay();
-            day = date.getDate();
-            month = date.getMonth();
-            year = date.getFullYear();
+            minutes = date.getMinutes(),
+            seconds = date.getSeconds(),
+            dayWeek = date.getDay(),
+            day = date.getDate(),
+            month = date.getMonth(),
+            year = date.getFullYear();  
 
 
-        var pHours  = document.getElementById('Hours'),
+        var pHours  = document.getElementById('hours'),
             pAMPM = document.getElementById('ampm'),
             pMinutes = document.getElementById('minutes'),
             pSeconds = document.getElementById('seconds'),
@@ -29,8 +29,34 @@
         pMonth.textContent = months[month];
 
         pYear.textContent = year;
+
+        if(hours >= 12) {
+            hours = hours -12;
+            ampm = 'PM';
+        } else {
+            ampm = 'AM';
+        }
+
+        if(hours == 0) {
+            hours = 12;
+        };
+
+        pHours.textContent = hours;
+        pAMPM.textContent = ampm;
+
+        if(minutes <10) {
+            minutes = '0' + minutes;
+        }
+
+        if(seconds <10) {
+            seconds = '0' + seconds;
+        }
+
+        pMinutes.textContent = minutes;
+        pSeconds.textContent = seconds;
     };
 
     updateHour();
+    var interval = setInterval(updateHour, 1000);
 
 }())
